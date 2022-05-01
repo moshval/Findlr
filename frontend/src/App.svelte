f163e407e69166c6ffcfbd20f1f54ce5d74ea25c
moshval <shevamohammad10@gmail.com>
2022-05-01 10:05:19 +0700

    add frontend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,33 +3,56 @@
 	let glet = Array(5);
 	let ylet = Array(5);
 	let xlet = '';
-	
-    let todoList = [{text: 'Write my first post', status: true},
-                    {text: 'Upload the post to the blog', status: false},
-                    {text: 'Publish the post at Facebook', status: false}];
+	let wordList = [];
+	let wordList2 = [];
+	let lan = 0;
 	
 	function addToList() {
-		todoList = [...todoList, {text: newItem, status: false}];
-		newItem = '';
+		if(wordList == null || wordList.length == 0) {
+			wordList2 = [];
+			return
+		}
+		wordList2 = []
+		console.log("called")
+		for (let i = 0; i < wordList.length; i++) {
+			let obj = {text: wordList[i], status: false};
+			wordList2.push(obj);
+		}
+		console.log(wordList2);
 	}
-	
+	function changeLang(){
+		let lg = document.getElementById("lang")
+		console.log(lg.value)
+		if(lg.value == "en"){
+			lan = 0;
+		}
+		else if(lg.value == "id"){
+			lan = 1;
+		}
+	}
 	function removeFromList(index) {
-		todoList.splice(index, 1)
-		todoList = todoList;
+		wordList2.splice(index, 1)
+		wordList2 = wordList2;
     }
 	async function fetchWords(lang,green,yellow,exc){
+		let gr = [];
+		let yl = [];
 		for(let i=0;i<5;i++){
-			if(green[i] == ''){
-				green[i] = '_';
+			gr[i] = green[i];
+			yl[i] = yellow[i];
+			if(green[i] == '' || green[i] == null){
+				gr[i] = '_';
 			}
-			if(yellow[i] == ''){
-				yellow[i] = '_';
+			if(yellow[i] == '' || yellow[i] == null){
+				yl[i] = '_';
 			}
 		}
-		let grn = green.join('');
+		let grn = gr.join('');
 		console.log(grn);
-		let ylw = yellow.join('');
+		let ylw = yl.join('');
 		console.log(ylw);
+		grn = grn.toLowerCase();
+		ylw = ylw.toLowerCase();
 		exc = exc.replace(/\s/g, "")
 		let url;
 		if(exc.length > 0){
@@ -38,18 +61,23 @@
 		else{
 			url = 'https://findlr-bend.herokuapp.com/words?'+'type='+lang+'&word='+grn+'&bad='+ylw;
 		}
-		let response = await fetch(url,{
-			method:'GET',headers:{
-				'Content-Type': 'application/json'
-			}
-		});
-		console.log(response.json())
-		return response.json();
+		console.log(url)
+		let response = await fetch(url)
+    		.then(response => response.json())  // convert to json
+    		.then(json => wordList = json.words)    //print data to console
+    		.catch(err => console.log('Request Failed', err)); // Catch error
+		addToList();
 	}
 </script>
 <div class="container">
+	<div class="container2">
 	<div class="sub-container">
 	<h1 style="text-align:center">Findlr</h1>
+	<h2>Lang</h2>
+	<select id="lang" on:change={() => changeLang()}>
+		<option value="en">English</option>
+		<option value="id">Indonesian</option>
+	</select>
 	<h2>Green Letters</h2>
 	<div class="green-letters">
 		<input class="colored-letter" bind:value={glet[0]} type="text" maxlength="1" placeholder="">
@@ -69,15 +97,20 @@
 	<h2>Excluded Letters</h2>
 		<input class="excluded-letter" bind:value={xlet} type="text" placeholder="">
 	<br>
-	<button on:click={fetchWords(0,glet,ylet,xlet)}>Submit</button>
-
-	<br/>
-	{#each todoList as item, index}
+	<button class="sbmt-btn" on:click={fetchWords(lan,glet,ylet,xlet)}>Submit</button>
+	<h2>Possible Answer(s)</h2>
+	<div class ="answers">
+	<ul>
+	{#each wordList2 as item, index}
+		<li>
 		<input bind:checked={item.status} type="checkbox">
 		<span class:checked={item.status}>{item.text}</span>
-		<span on:click={() => removeFromList(index)}>❌</span>
-		<br/>
-	{/each} 
+		<span id="del-item" on:click={() => removeFromList(index)}>❌</span>
+		</li>
+	{/each}
+	</ul>	
+	</div>
+	</div>
 	</div>
 </div>
 
@@ -89,11 +122,37 @@
 	.green-letters{
 		display: flex;
 	}
+	.green-letters .colored-letter{
+		background-color: #40a375;
+		color: white;
+	}
+	.yellow-letters .colored-letter{
+		background-color: #ffba01;
+		color: black;
+	}
 	.yellow-letters{
 		display: flex;
 	}
 	.excluded-letter{
 		width: 16rem;
+	}
+	ul{
+		list-style: none;
+		display: block;
+		padding-left: 0;
+		padding-top: 0;
+		-moz-column-count: 2;
+		-moz-column-gap: 10px;
+		-webkit-column-count: 2;
+		-webkit-column-gap: 10px;
+		column-count: 2;
+		column-gap: 10px;
+	}
+	.sbmt-btn{
+		width: 16rem;
+	}
+	.sbmt-btn:hover{
+		cursor: pointer;
 	}
 	.colored-letter{
 		border: 2px solid gray;
@@ -116,4 +175,15 @@
 		align-items: center;
 		justify-content: center;
 	}
+	.container2{
+		border-radius: 20px;
+	}
+	.sub-container{
+		align-items: center;
+		padding: 4rem;
+	}
+	#del-item{
+		cursor: pointer;
+	}
+
 </style> 
